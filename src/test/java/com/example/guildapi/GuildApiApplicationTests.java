5f02c40ad797992437f84019c964dcbafa337fe8
Leticia-SS <leticia.saraiva013@gmail.com>
2026-03-02 13:20:50 -0300

    initial commit

---
+++ src/test/java/com/example/guildapi/GuildApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guildapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
