dbc0796a7f1dea8c6ba3261058d00d7ca464fc09
Leticia-SS <leticia.saraiva013@gmail.com>
2026-03-02 18:25:47 -0300

    create GET methods

---
+++ src/main/java/com/example/guildapi/controller/AventureiroController.java
@@ -0,0 +1,40 @@
+package com.example.guildapi.controller;
+
+import com.example.guildapi.model.Aventureiro;
+import com.example.guildapi.service.AventureiroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/aventureiros")
+public class AventureiroController {
+    private final AventureiroService aventureiroService;
+
+    public AventureiroController(AventureiroService aventureiroService) {
+        this.aventureiroService = aventureiroService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Aventureiro>> listarTodos(){
+        List<Aventureiro> aventureiros = aventureiroService.listarAventureiros();
+        return ResponseEntity.ok(aventureiros);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> listarAventureiro(@PathVariable Integer id) {
+        Optional<Aventureiro> aventureiro = aventureiroService.listarAventureiroPorId(id);
+        if (aventureiro.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro: aventureiro não encontrado");
+        }
+        return ResponseEntity.ok(aventureiro.get());
+    }
+
+
+}
