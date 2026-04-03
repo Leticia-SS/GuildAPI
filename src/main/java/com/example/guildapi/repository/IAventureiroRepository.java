a3d148b8841e456803dc462ab1bb3323a9adea23
Leticia-SS <leticia.saraiva013@gmail.com>
2026-04-03 19:53:00 -0300

    ajust aventureiroRepository

--- src/main/java/com/example/guildapi/repository/IAventureiroRepository.java
+++ src/main/java/com/example/guildapi/repository/IAventureiroRepository.java
@@ -1,7 +1,13 @@
 package com.example.guildapi.repository;
 
 import com.example.guildapi.model.aventura.Aventureiro;
+import com.example.guildapi.model.enums.ClasseEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IAventureiroRepository extends JpaRepository<Aventureiro, Long> {
+    Page<Aventureiro> findByClasse(ClasseEnum classe, Pageable pageable);
+    Page<Aventureiro> findByAtivo(boolean ativo, Pageable pageable);
+    Page<Aventureiro> findByNivelGreaterThanEqual(Integer nivelMinimo, Pageable pageable);
 }
